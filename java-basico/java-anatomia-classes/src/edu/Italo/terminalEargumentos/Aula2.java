4ff308c372e8a9e0d335271301a3c0d3ad713ab2
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-08-24 14:53:13 -0300

    subindo aulas terminal e argumentos

---
+++ java-basico/java-anatomia-classes/src/edu/Italo/terminalEargumentos/Aula2.java
@@ -0,0 +1,7 @@
+package edu.Italo.terminalEargumentos;
+
+public class Aula2 {
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo terminal");
+    }
+}
