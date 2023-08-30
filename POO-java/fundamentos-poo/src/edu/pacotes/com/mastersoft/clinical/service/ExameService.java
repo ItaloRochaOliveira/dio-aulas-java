421e0c252ed423902b7513fff67f770950c2c2ec
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-08-30 13:45:59 -0300

    subindo aulas POO pacotes

---
+++ POO-java/fundamentos-poo/src/edu/pacotes/com/mastersoft/clinical/service/ExameService.java
@@ -0,0 +1,9 @@
+package edu.pacotes.com.mastersoft.clinical.service;
+
+import edu.pacotes.com.mastersoft.clinical.model.Exame;
+
+public class ExameService {
+    void salvarExame(Exame exame){
+
+    }
+}
