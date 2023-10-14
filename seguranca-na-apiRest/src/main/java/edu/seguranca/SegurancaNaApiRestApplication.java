961f4deee4ee26cd9d35492c6a2b6b103b302f82
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-10-13 23:11:15 -0300

    subindo aulas segurança na api

---
+++ seguranca-na-apiRest/src/main/java/edu/seguranca/SegurancaNaApiRestApplication.java
@@ -0,0 +1,12 @@
+package edu.seguranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegurancaNaApiRestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SegurancaNaApiRestApplication.class, args);
+	}
+
+}
---
+++ seguranca-na-apiRest/src/test/java/edu/seguranca/SegurancaNaApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.seguranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegurancaNaApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
