c4fee5a2eb4241dc405c069155499b4c6171a2db
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-10-17 00:14:42 -0300

    subindo aulas sobre seguranca na api - JWT

---
+++ seguranca-na-apiRest/jwt/src/main/java/edu/jwt/service/UserService.java
@@ -0,0 +1,25 @@
+package edu.jwt.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import edu.jwt.model.User;
+import edu.jwt.repository.UserRepository;
+
+
+@Service
+@Component
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private PasswordEncoder encoder;
+    public void createUser(User user){
+        String pass = user.getPassword();
+        //criptografando antes de salvar no banco
+        user.setPassword(encoder.encode(pass));
+        repository.save(user);
+    }
+}
